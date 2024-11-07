65d6bb431436b64661527ec00cbc8a55c92884e1
TALIS_DEV <wordbit.bool@gmail.com>
2024-11-07 20:34:56 -0300

    feat: update fonts to mulish

--- app/page.tsx
+++ app/page.tsx
@@ -3,27 +3,20 @@ import { Button } from "./_components/ui/button";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-
 const Home = async () => {
+  const { userId } = await auth();
 
-  const {userId} = await auth(); 
-
-  if(!userId){
-   redirect("/login")
+  if (!userId) {
+    redirect("/login");
   }
 
-  return (  
-    <div className="flex w-screen justify-center items-center "> 
-
-<h1 className="text-center text-xl "> hello world</h1>
-<Button> loguin </Button>
-<UserButton showName>
-
-</UserButton>
-
+  return (
+    <div className="flex w-screen justify-center items-center ">
+      <h1 className="text-center text-xl "> hello world</h1>
+      <Button> loguin </Button>
+      <UserButton showName></UserButton>
     </div>
-    
   );
-}
- 
+};
+
 export default Home;
