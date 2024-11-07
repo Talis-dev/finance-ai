0663c160a79cc0bfdaf3bda0f7d7163cc76c588d
TALIS_DEV <wordbit.bool@gmail.com>
2024-11-07 20:01:58 -0300

    feat: add clerk provider auth

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,18 @@
 import Image from "next/image";
 import { Button } from "../_components/ui/button";
 import { LogInIcon } from "lucide-react";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const LoginPage = () => {
+const LoginPage = async () => {
+
+ const {userId} = await auth();
+
+ if(userId){
+  redirect("/")
+ }
+
   return (
     <div className="grid grid-cols-2 h-full ">
       <div className="flex flex-col h-full justify-center p-8 max-w-[550px] mx-auto">
@@ -18,8 +28,10 @@ const LoginPage = () => {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
         </p>
-        <Button variant="outline" > <LogInIcon className="mr-2" />
+        <SignInButton>
+              <Button variant="outline" > <LogInIcon className="mr-2" />
             Fazer loguin ou criar conta</Button>
+        </SignInButton>
       </div>
 
       <div className="relative h-full w-full ">
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,29 @@
+import { UserButton } from "@clerk/nextjs";
 import { Button } from "./_components/ui/button";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const home = () => {
+
+const Home = async () => {
+
+  const {userId} = await auth(); 
+
+  if(!userId){
+   redirect("/login")
+  }
+
   return (  
     <div className="flex w-screen justify-center items-center "> 
 
 <h1 className="text-center text-xl "> hello world</h1>
 <Button> loguin </Button>
+<UserButton showName>
+
+</UserButton>
 
     </div>
     
   );
 }
  
-export default home;
+export default Home;
