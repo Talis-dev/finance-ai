28b034fcacfe1f1c7bdf127430c9c0d086b57106
TALIS_DEV <wordbit.bool@gmail.com>
2024-11-07 19:01:02 -0300

    chore add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
+import { Button } from "./_components/ui/button";
+
 const home = () => {
   return (  
-    <h1> hello world</h1>
+    <div className="flex w-screen justify-center items-center "> 
+
+<h1 className="text-center text-xl "> hello world</h1>
+<Button> loguin </Button>
+
+    </div>
+    
   );
 }
  
